ff27b2888ae964004f84ce44c0c4ce66157d1690
Ehsan-1998 <Faizeeehsan@gmail.com>
2024-11-23 14:55:17 -0500

    Add appropriate fields , constructor and methods

--- src/main/java/com/example/project/Model/Ticket.java
+++ src/main/java/com/example/project/Model/Ticket.java
@@ -1,4 +1,46 @@
 package com.example.project.Model;
 
+import java.time.LocalDateTime;
+
 public class Ticket {
+
+    private int id;
+
+
+    private LocalDateTime  purchaseDateTime;
+
+
+    private Showtime showtime;
+
+
+    public Ticket(int id, LocalDateTime purchaseDateTime, Showtime showtime) {
+        this.id = id;
+        this.purchaseDateTime = purchaseDateTime;
+        this.showtime = showtime;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public LocalDateTime getPurchaseDateTime() {
+        return purchaseDateTime;
+    }
+
+    public void setPurchaseDateTime(LocalDateTime purchaseDateTime) {
+        this.purchaseDateTime = purchaseDateTime;
+    }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(Showtime showtime) {
+        this.showtime = showtime;
+    }
 }
