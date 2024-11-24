56ad87971ea48af8cfcb2c5f0f248acd19ebf18e
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2024-11-23 20:57:55 -0500

    javadoc MovieCell Class

--- src/main/java/Helpers/MovieCell.java
+++ src/main/java/Helpers/MovieCell.java
@@ -3,15 +3,29 @@ package Helpers;
 import Model.Movie;
 import javafx.scene.control.ListCell;
 
+/**
+ * Custom ListCell implementation for displaying movie details in a ListView.
+ * This class customizes the appearance of each cell in the ListView to show the
+ * title and genre of a movie.
+ */
 public class MovieCell extends ListCell<Movie> {
 
+
+    /**
+     * Updates the content of a ListCell for the given Movie object.
+     *
+     * @param pMovie The Movie object to display in the cell. If null, the cell is cleared.
+     * @param pEmpty A boolean indicating whether the cell is empty. If true, the cell is cleared.
+     */
     @Override
     protected void updateItem(Movie pMovie, boolean pEmpty) {
         super.updateItem(pMovie, pEmpty);
 
         if (pEmpty || pMovie == null) {
+            // Clear the cell content if empty or no movie is provided
             setText(null);
         } else {
+            // Display the movie title and genre in the cell
             setText(pMovie.getTitle() + " - " + pMovie.getGenre());
         }
     }
