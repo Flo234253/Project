80861578251920f349ff0a999ec32a20968b3d5d
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2024-11-23 20:50:01 -0500

    javadoc for the ConsultMovieController

--- src/main/java/com/example/project/Controllers/ConsultMovieController.java
+++ src/main/java/com/example/project/Controllers/ConsultMovieController.java
@@ -4,6 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+/**
+ * Controller class for the "Consult Movie" view.
+ * This class manages the display of detailed movie information
+ * and provides functionality to close the view.
+ */
 public class ConsultMovieController {
 
     @FXML
@@ -29,11 +34,26 @@ public class ConsultMovieController {
 
     private Stage aStage;
 
-
+    /**
+     * Sets the stage for the view. The stage is used to control the window.
+     *
+     * @param pStage The stage to set.
+     */
     public void setStage(Stage pStage) {
         this.aStage = pStage;
     }
 
+    /**
+     * Populates the text fields with the details of the movie.
+     *
+     * @param pTitle       The title of the movie.
+     * @param pGenre       The genre of the movie.
+     * @param pReleaseDate The release date of the movie.
+     * @param pDuration    The duration of the movie.
+     * @param pActors      The main actors of the movie.
+     * @param pDirector    The director of the movie.
+     * @param pDescription The description of the movie.
+     */
     public void setMovieDetails(String pTitle, String pGenre, String pReleaseDate, String pDuration, String pActors, String pDirector, String pDescription) {
         aTitleField.setText(pTitle);
         aGenreField.setText(pGenre);
@@ -44,6 +64,10 @@ public class ConsultMovieController {
         aDescriptionArea.setText(pDescription);
     }
 
+    /**
+     * Closes the "Consult Movie" view.
+     * This method is triggered by the "Close" button in the view.
+     */
     @FXML
     private void onCloseButtonClicked() {
         if (aStage != null) {
