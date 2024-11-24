95871524c3af85d85987c89ca89b542c2bf5b821
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2024-11-23 22:24:02 -0500

    -confirmation when i want to delete a movies from the listview
    when i delete movies from the listview it give me a message to confirm if i want to delete that movies. If it yes, it give me a message of confirmation that movie was deleted successfully and give the name of the movies deleted

--- src/main/java/Helpers/AlertHelper.java
+++ src/main/java/Helpers/AlertHelper.java
@@ -1,4 +1,57 @@
 package Helpers;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
+import java.util.Optional;
+
+/**
+ *  class for creating and displaying alerts.
+ */
 public class AlertHelper {
+
+    /**
+     * Displays a confirmation alert with the given title, header, and content
+     *
+     * @param title   The title of the alert.
+     * @param header  The header text of the alert.
+     * @param content The content text of the alert.
+     * @return An Optional containing the user's response.
+     */
+    public static Optional<ButtonType> showConfirmationAlert(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        return alert.showAndWait();
+    }
+
+    /**
+     * Displays an information alert with the given title, header, and content
+     *
+     * @param title   The title of the alert.
+     * @param header  The header text of the alert (can be null).
+     * @param content The content text of the alert.
+     */
+    public static void showInformationAlert(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    /**
+     * Displays a warning alert with the given title, header, and content.
+     *
+     * @param title   The title of the alert.
+     * @param header  The header text of the alert (can be null).
+     * @param content The content text of the alert.
+     */
+    public static void showWarningAlert(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
