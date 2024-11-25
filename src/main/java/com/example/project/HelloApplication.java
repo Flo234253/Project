2a58d527be751c2fbd226e78e1b4f026c6a95654
Ehsan-1998 <Faizeeehsan@gmail.com>
2024-11-25 10:08:12 -0500

    changed the view

--- src/main/java/com/example/project/HelloApplication.java
+++ src/main/java/com/example/project/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-              FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SignInClient.fxml"));
+              FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("movie-list-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 550, 550);
         stage.setTitle("Welcome to YourCinema Login!");
         stage.setScene(scene);
