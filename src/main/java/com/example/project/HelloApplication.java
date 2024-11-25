14d9246bf9958a74f53e29493f69570479ce3ecf
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2024-11-25 10:36:44 -0500

    changement

--- src/main/java/com/example/project/HelloApplication.java
+++ src/main/java/com/example/project/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("screen-room-list-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("movie-list-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 550);
         stage.setTitle("Welcome to Manager Login!");
         stage.setScene(scene);
