13b5d0514c47b0f5dff82d23df74fb2c89296a14
Ehsan-1998 <Faizeeehsan@gmail.com>
2024-11-27 14:05:14 -0500

    Updated the controllers and scenes

--- src/main/java/com/example/project/Controllers/ConsultShowTimeDetailsViewController.java
+++ src/main/java/com/example/project/Controllers/ConsultShowTimeDetailsViewController.java
@@ -1,11 +1,84 @@
 package com.example.project.Controllers;
 
 
+import com.example.project.Model.ShowTime;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 /**
  * Controller class for the Consult ShowTime Details view.
  *
  */
 public class ConsultShowTimeDetailsViewController {
-//Todo
 
+    /**
+     * TableView displaying the list of showtimes.
+     * Each row represents a single showtime entry.
+     */
+    @FXML
+    private TableView<ShowTime> showTimeTable;
+
+    /**
+     * TableColumn displaying the ShowTime ID for each showtime.
+     * Maps to the `showtimeId` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, Integer> showTimeIdColumn;
+
+    /**
+     * TableColumn displaying the Date for each showtime.
+     * Maps to the `date` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, String> dateColumn;
+
+    /**
+     * TableColumn displaying the Time for each showtime.
+     * Maps to the `time` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, String> timeColumn;
+
+    /**
+     * TableColumn displaying the Room ID for each showtime.
+     * Maps to the `roomId` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, Integer> roomIdColumn;
+
+    /**
+     * TableColumn displaying the Movie ID for each showtime.
+     * Maps to the `movieId` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, Integer> movieIdColumn;
+
+    /**
+     * TableColumn displaying whether the showtime is Full or not.
+     * Maps to the `full` property in the ShowTime object.
+     */
+    @FXML
+    private TableColumn<ShowTime, Boolean> fullColumn;
+
+    /**
+     * Button that allows the user to go back to the previous screen.
+     * This button can be used to navigate or perform other actions.
+     */
+    @FXML
+    private Button backButton;
+
+    /**
+     * Handles the back button click event.
+     * This method can be used to navigate back to the previous screen
+     * or to perform other actions associated with the back button.
+     *
+     * @param actionEvent The event that triggered the method.
+     */
+    @FXML
+    private void handleBackButton(ActionEvent actionEvent) {
+        // Code to handle the back button click event
+    }
 }
--- src/main/java/com/example/project/Controllers/ManageShowTimeViewController.java
+++ src/main/java/com/example/project/Controllers/ManageShowTimeViewController.java
@@ -1,11 +1,140 @@
 package com.example.project.Controllers;
 
-/**
- * Controller class for the Manage ShowTime view.
-*/
+import com.example.project.Model.ShowTime;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-// TO Do
+/**
+ * Controller class for managing the movie list view.
+ * Provides functionality for searching, consulting, adding, editing, and deleting movies.
+ */
 public class ManageShowTimeViewController {
 
+
+    /**
+     * Button for refreshing the movie list.
+     */
+    @FXML
+    private Button searchButton;
+
+    /**
+     * TextField for searching movies in the list by title or genre.
+     */
+    @FXML
+    private TextField searchField;
+
+
+    /**
+     * TableView for displaying the list of showtime.
+     */
+    @FXML
+    private TableView<ShowTime> showTimeTableView;
+
+
+    /**
+     * TableColumn for displaying movie titles in the TableView.
+     */
+    @FXML
+    private TableColumn<ShowTime, String> movieColumn;
+
+
+    /**
+     * TableColumn for displaying showtime  in the TableView.
+     */
+    @FXML
+    private TableColumn<ShowTime, String> showTimeColumn;
+
+
+    /**
+     * Button for adding a showtime to the list.
+     */
+    @FXML
+    private Button addShowTimeButton;
+
+
+    /**
+     * Button for deleting the selected Showtime from the table.
+     */
+    @FXML
+    private Button deleteShowTimeButton;
+
+    /**
+     * Button for consulting detailed information about Showtime.
+     */
+    @FXML
+    private Button consultShowTimeButton;
+
+    /**
+     * Button for going back to previous page.
+     */
+    @FXML
+    private Button backButton;
+
+
+
+    /**
+     * Filters movies based on the input from the search field.
+     * <p>
+     * If no movies match the input, an alert is displayed to the manager.
+     * </p>
+     */
+    @FXML
+    private void handleSearchButton(ActionEvent actionEvent) {
+
+
+
+    }
+
+
+    /**
+     * Opens a new window for adding a new showtime to the list.
+     */
+    @FXML
+    private void handleAddButton(ActionEvent actionEvent) {
+
+
+
+    }
+
+
+
+    /**
+     * Deletes the selected showtime from the list.
+     * <p>
+     * Displays a confirmation alert before deletion.
+     * </p>
+     */
+    @FXML
+    private void handleDeleteButton(ActionEvent actionEvent) {
+
+
+
+    }
+
+
+
+    /**
+     * Opens a new window to display the information about the selected showtime.
+     */
+    @FXML
+    private void handleConsultButton(ActionEvent actionEvent) {
+
+
+    }
+
+
+    /**
+     *
+     * to witch back to the previous view
+     */
+
+    private void handleBackButton(ActionEvent actionEvent) {
+
+
+    }
 
 }
