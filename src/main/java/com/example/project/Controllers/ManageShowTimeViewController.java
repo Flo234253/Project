8c45df53b93402d6c101da249b0a92bbc3ff7764
Ehsan-1998 <Faizeeehsan@gmail.com>
2024-11-28 16:30:34 -0500

    updated the showtimeview.fxml and controller.

--- src/main/java/com/example/project/Controllers/ManageShowTimeViewController.java
+++ src/main/java/com/example/project/Controllers/ManageShowTimeViewController.java
@@ -7,15 +7,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
--- src/main/java/com/example/project/Controllers/ModifyShowTimeViewController.java
+++ src/main/java/com/example/project/Controllers/ModifyShowTimeViewController.java
@@ -1,4 +1,56 @@
 package com.example.project.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class ModifyShowTimeViewController {
+
+
+    public DatePicker dateField;
+    @FXML
+    private TextField showTimeIdField;
+
+
+
+
+
+    @FXML
+    private TextField timeField;
+
+
+
+    @FXML
+    private TextField roomIdField;
+
+
+    @FXML
+    private TextField movieIdField;
+
+
+    @FXML
+    private Button modifyShowTimeButton;
+
+
+    @FXML
+    private Button cancelButton;
+
+
+    @FXML
+    private CheckBox fullCheckBox;
+
+    public void handleModifyShowTimeButton(ActionEvent actionEvent) {
+
+
+
+    }
+
+    public void handleCancelButton(ActionEvent actionEvent) {
+
+
+
+    }
 }
