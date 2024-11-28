73eeaf326ec77c0d722a0063679d73151e77ea17
Flo234253 <florenceler3@gmail.com>
2024-11-28 09:23:47 -0500

    Draft of my buy ticket

---
+++ src/main/java/com/example/project/Controllers/BuyingTicketController.java
@@ -0,0 +1,15 @@
+package com.example.project.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class BuyingTicketController {
+    public DatePicker datePicker;
+    public ListView moveAndShowtimeListView;
+    public TextField numberTicketTextField;
+
+    public void buyButton(ActionEvent event) {
+    }
+}
